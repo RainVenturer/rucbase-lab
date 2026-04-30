4923ca6eb4eb2742de0b229fba99850d0af44027
RainVenturer <maoyu3091701340@gmail.com>
2026-04-30 14:47:21 +0800

    Finish lab1 work

--- src/record/rm_scan.cpp
+++ src/record/rm_scan.cpp
@@ -9,39 +9,52 @@ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
 #include "rm_scan.h"
+
 #include "rm_file_handle.h"
 
 /**
  * @brief 初始化file_handle和rid
  * @param file_handle
  */
-RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
-    // Todo:
-    // 初始化file_handle和rid（指向第一个存放了记录的位置）
-
+RmScan::RmScan(const RmFileHandle* file_handle) : file_handle_(file_handle) {
+    rid_ = {.page_no = RM_NO_PAGE, .slot_no = RM_NO_PAGE};
+    for (int page_no = RM_FIRST_RECORD_PAGE; page_no < file_handle_->file_hdr_.num_pages; page_no++) {
+        auto page_handle = file_handle_->fetch_page_handle(page_no);
+        int slot_no = Bitmap::first_bit(true, page_handle.bitmap, file_handle_->file_hdr_.num_records_per_page);
+        file_handle_->buffer_pool_manager_->unpin_page(page_handle.page->get_page_id(), false);
+        if (slot_no < file_handle_->file_hdr_.num_records_per_page) {
+            rid_ = {.page_no = page_no, .slot_no = slot_no};
+            return;
+        }
+    }
 }
 
 /**
  * @brief 找到文件中下一个存放了记录的位置
  */
 void RmScan::next() {
-    // Todo:
-    // 找到文件中下一个存放了记录的非空闲位置，用rid_来指向这个位置
-
+    if (is_end()) {
+        return;
+    }
+    for (int page_no = rid_.page_no; page_no < file_handle_->file_hdr_.num_pages; page_no++) {
+        auto page_handle = file_handle_->fetch_page_handle(page_no);
+        int curr = (page_no == rid_.page_no) ? rid_.slot_no : -1;
+        int slot_no = Bitmap::next_bit(true, page_handle.bitmap, file_handle_->file_hdr_.num_records_per_page, curr);
+        file_handle_->buffer_pool_manager_->unpin_page(page_handle.page->get_page_id(), false);
+        if (slot_no < file_handle_->file_hdr_.num_records_per_page) {
+            rid_ = {.page_no = page_no, .slot_no = slot_no};
+            return;
+        }
+    }
+    rid_ = {.page_no = RM_NO_PAGE, .slot_no = RM_NO_PAGE};
 }
 
 /**
  * @brief ​ 判断是否到达文件末尾
  */
-bool RmScan::is_end() const {
-    // Todo: 修改返回值
-
-    return false;
-}
+bool RmScan::is_end() const { return rid_.page_no == RM_NO_PAGE; }
 
 /**
  * @brief RmScan内部存放的rid
  */
-Rid RmScan::rid() const {
-    return rid_;
-}
+Rid RmScan::rid() const { return rid_; }
